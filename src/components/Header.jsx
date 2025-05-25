812aaa47df7eccb13fba76a09d0af79a44c1115a
kazari4 <skidyuk86115@gmail.com>
2025-05-26 00:38:49 +0900

    ヘッダーを作成

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,13 @@
 function Header() {
-  return
+  return (
+    <header className="hero is-info is-small">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title has-text-weight-bold">Iris dataset scatter plot</h1>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;

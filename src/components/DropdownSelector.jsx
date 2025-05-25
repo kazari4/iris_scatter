6968c492d92371f0f931c2e8f13a6c51395f68f8
kazari4 <skidyuk86115@gmail.com>
2025-05-26 01:26:49 +0900

    ドロップボタンを追加

--- src/components/DropdownSelector.jsx
+++ src/components/DropdownSelector.jsx
@@ -1,5 +1,39 @@
 function DropdownSelector() {
-  return;
+  return (
+    <div className="columns">
+      <div className="column">
+        <div className="field">
+          <label className="label">X Axis</label>
+          <div className="control">
+            <div className="select">
+              <select defaultValue="sepal length">
+                <option>sepal length</option>
+                <option>sepal width</option>
+                <option>petal length</option>
+                <option>petal width</option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="column">
+        <div className="field">
+          <label className="label">Y Axis</label>
+          <div className="control">
+            <div className="select">
+              <select defaultValue="sepal width">
+                <option>sepal length</option>
+                <option>sepal width</option>
+                <option>petal length</option>
+                <option>petal width</option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default DropdownSelector;

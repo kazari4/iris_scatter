03233e04a615b2b3720e42da5f9b4a9ab768b7f2
kazari4 <skidyuk86115@gmail.com>
2025-05-26 16:57:25 +0900

    api.jsを作成

---
+++ src/api.js
@@ -0,0 +1,7 @@
+export async function fetchIrisDataSet() {
+  const response = await fetch(
+    `https://s3-us-west-2.amazonaws.com/s.cdpn.io/2004014/iris.json`
+  );
+  const data = await response.json();
+  return data;
+}

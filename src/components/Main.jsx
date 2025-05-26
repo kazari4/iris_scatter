f17496c7affc040eeae75db85bbf35fc8add0b01
kazari4 <skidyuk86115@gmail.com>
2025-05-26 17:49:48 +0900

    Loading画面の実装

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { fetchIrisDataSet } from "../api";
 import DropdownSelector from "./DropdownSelector/DropdownSelector";
 import ScatterPlot from "./ScatterPlot/ScatterPlot";
+import Loading from "./Loading/Loading";
 
 function Main() {
   const [irisData, setIrisData] = useState(null);
@@ -10,6 +11,12 @@ function Main() {
       setIrisData(irisData);
     });
   }, []);
+
+  if (irisData === null) {
+    return (
+      <Loading />
+    )
+  }
   return (
     <section className="section">
       <DropdownSelector />

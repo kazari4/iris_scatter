d955611cc1608e210d5b8ef82833a1151a473900
kazari4 <skidyuk86115@gmail.com>
2025-05-27 16:30:00 +0900

    データを整形する関数を作成

---
+++ src/components/convertData.js
@@ -0,0 +1,17 @@
+export function convertData(data, xLabel, yLabel) {
+  const axisKeyConvert = {
+    "sepal length": "sepalLength",
+    "sepal width": "sepalWidth",
+    "petal length": "petalLength",
+    "petal width": "petalWidth",
+  };
+
+  const xKey = axisKeyConvert[xLabel];
+  const yKey = axisKeyConvert[yLabel];
+
+  return data.map((d) => ({
+    x: d[xKey],
+    y: d[yKey],
+    species: d.species,
+  }));
+}

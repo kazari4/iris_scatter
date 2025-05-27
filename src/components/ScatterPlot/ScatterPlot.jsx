b5e95aac9d6d0c386edcd0bb2a15b946f150c403
kazari4 <skidyuk86115@gmail.com>
2025-05-27 16:45:20 +0900

    convertedDataをもとにscaleを作るよう変更

--- src/components/ScatterPlot/ScatterPlot.jsx
+++ src/components/ScatterPlot/ScatterPlot.jsx
@@ -1,18 +1,14 @@
-import { getAxisKey } from "./getAxisKey"
 import { createScale } from "./createScale"
 
-function ScatterPlot({ data, xAxis, yAxis }) {
+function ScatterPlot(data) {
   const width = 800;
   const height = 800;
-
-  const xKey = getAxisKey(xAxis);
-  const yKey = getAxisKey(yAxis);
 
   const xRange = [0, 500]
   const yRange = [500, 0]
 
-  const xScale = createScale(data, xKey, xRange)
-  const yScale = createScale(data, yKey, yRange)
+  const xScale = createScale(data, x, xRange)
+  const yScale = createScale(data, y, yRange)
 
 
   return (

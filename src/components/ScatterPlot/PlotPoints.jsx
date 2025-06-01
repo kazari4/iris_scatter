ac1ad844cd64f8363819d935feb7e1f8e1e10dbd
kazari4 <skidyuk86115@gmail.com>
2025-06-01 20:09:18 +0900

    点と仮置き用の枠を表示するよう変更

--- src/components/ScatterPlot/PlotPoints.jsx
+++ src/components/ScatterPlot/PlotPoints.jsx
@@ -1,11 +1,15 @@
 function PlotPoints({ data, xScale, yScale, color }) {
   return (
-    <g transform={`translate(50,550) scale(1,-1)`}>
-      <line x1="-100" y1="0" x2="800" y2="0" stroke="white" />
-      <line x1="0" y1="-100" x2="0" y2="800" stroke="white" />
-      <line x1="500" y1="-100" x2="500" y2="800" stroke="white" />
-      <line x1="-100" y1="500" x2="800" y2="500" stroke="white" />
-      <circle cx="0" cy="0" r="5" fill="red" />
+    <g transform={`translate(50,450) scale(1,-1)`}>
+      <line x1="-50" y1="0" x2="600" y2="0" stroke="white" />
+      <line x1="0" y1="-50" x2="0" y2="600" stroke="white" />
+      <line x1="400" y1="-100" x2="400" y2="800" stroke="white" />
+      <line x1="-50" y1="400" x2="600" y2="400" stroke="white" />
+      {data.map(({ x, y, species }, i) => {
+        return (
+          <circle key={i} cx={xScale(x)} cy={yScale(y)} r="5" fill={color[species]} />
+        )
+      })}
     </g>
   )
 }

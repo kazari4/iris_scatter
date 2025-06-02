5741e0f44d4d68e41d6915a65a3b2b94a29148b6
kazari4 <skidyuk86115@gmail.com>
2025-06-03 00:27:40 +0900

    凡例を表示するように変更

--- src/components/ScatterPlot/PlotLegend.jsx
+++ src/components/ScatterPlot/PlotLegend.jsx
@@ -1,5 +1,25 @@
-function PlotLegend() {
-  return
+function switchVisibleSpecies(setVisibleSpecies, speciesName) {
+  setVisibleSpecies(prevVisibleSpecies => ({
+    ...prevVisibleSpecies,
+    [speciesName]: !prevVisibleSpecies[speciesName]
+  }));
+}
+
+
+function PlotLegend({ speciesList, color, visibleSpecies, setVisibleSpecies }) {
+  const boxSize = 10
+  return (
+    <g transform="translate(400,50)">
+      {speciesList.map((species, i) => {
+        return (
+          <g key={i} transform={`translate(20,${30 * i})`} onClick={() => switchVisibleSpecies(setVisibleSpecies, species)}>
+            <rect x="0" y="0" width={boxSize} height={boxSize} fill={color[species]} />
+            <text x="15" y={boxSize} >{species}</text>
+          </g>
+        )
+      })}
+    </g>
+  )
 }
 
 export default PlotLegend;

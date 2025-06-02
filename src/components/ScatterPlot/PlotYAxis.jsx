08f9e671e8e1f67cfbd7d5a937f97bd9ddfff83e
kazari4 <skidyuk86115@gmail.com>
2025-06-02 14:16:29 +0900

    Y軸を表示するように変更

--- src/components/ScatterPlot/PlotYAxis.jsx
+++ src/components/ScatterPlot/PlotYAxis.jsx
@@ -1,5 +1,21 @@
-function PlotYAxis(data, yScale) {
-  return;
+function PlotYAxis({ scale, rangeMax }) {
+  const ticks = scale.ticks()
+  const tickHeight = 10
+  const tickX = 0
+  return (
+    <g>
+      <line x1="0" y1="0" x2="0" y2={rangeMax} stroke="black" />
+      {ticks.map((tick, i) => {
+        const tickY = scale(tick)
+        return (
+          <g key={i}>
+            <line x1={tickX} y1={tickY} x2={tickX - tickHeight} y2={tickY} stroke="black" />
+            <text x={tickX - tickHeight} y={tickY} textAnchor="end">{tick}</text>
+          </g>
+        )
+      })}
+    </g>
+  )
 }
 
 export default PlotYAxis;

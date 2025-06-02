1c2cced6074563a7bafa6b876c80808b53770c6c
kazari4 <skidyuk86115@gmail.com>
2025-06-02 14:07:35 +0900

    X軸を描画するよう変更

--- src/components/ScatterPlot/PlotXAxis.jsx
+++ src/components/ScatterPlot/PlotXAxis.jsx
@@ -1,5 +1,21 @@
-function PlotXAxis(data, scale) {
-  return;
+function PlotXAxis({ scale, rangeMax }) {
+  const ticks = scale.ticks()
+  const tickHeight = 10
+  const tickY = rangeMax
+  return (
+    <g>
+      <line x1="0" y1={rangeMax} x2={rangeMax} y2={rangeMax} stroke="black" />
+      {ticks.map((tick, i) => {
+        const tickX = scale(tick)
+        return (
+          <g key={i}>
+            <line x1={tickX} y1={tickY} x2={tickX} y2={tickY + tickHeight} stroke="black" />
+            <text x={tickX} y={tickY + tickHeight} textAnchor="middle" dominantBaseline="hanging">{tick}</text>
+          </g>
+        )
+      })}
+    </g>
+  )
 }
 
 export default PlotXAxis;

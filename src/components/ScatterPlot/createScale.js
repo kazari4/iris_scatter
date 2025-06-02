f5d0db91c2421800285b7c088819695443d508a3
kazari4 <skidyuk86115@gmail.com>
2025-06-02 14:06:15 +0900

    scaleにniceを適用

--- src/components/ScatterPlot/createScale.js
+++ src/components/ScatterPlot/createScale.js
@@ -3,5 +3,5 @@ import * as d3 from "d3";
 export function createScale(data, key, range) {
   const min = Math.min(...data.map(d => d[key]));
   const max = Math.max(...data.map(d => d[key]));
-  return d3.scaleLinear().domain([min, max]).range(range);
+  return d3.scaleLinear().domain([min, max]).range(range).nice();
 }

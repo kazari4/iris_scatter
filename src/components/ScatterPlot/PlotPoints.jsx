38f7f92097562a567ea14ad8eb48f478206159b7
kazari4 <skidyuk86115@gmail.com>
2025-06-02 18:43:43 +0900

    枠線を削除

--- src/components/ScatterPlot/PlotPoints.jsx
+++ src/components/ScatterPlot/PlotPoints.jsx
@@ -1,10 +1,6 @@
 function PlotPoints({ data, xScale, yScale, color }) {
   return (
     <g>
-      <line x1="-50" y1="0" x2="600" y2="0" stroke="white" />
-      <line x1="0" y1="-50" x2="0" y2="600" stroke="white" />
-      <line x1="400" y1="-100" x2="400" y2="800" stroke="white" />
-      <line x1="-50" y1="400" x2="600" y2="400" stroke="white" />
       {data.map(({ x, y, species }, i) => {
         return (
           <circle key={i} cx={xScale(x)} cy={yScale(y)} r="5" fill={color[species]} />

0ca91679958582bed3ff858e24ad336c666ec878
kazari4 <skidyuk86115@gmail.com>
2025-06-03 01:42:25 +0900

    点を削除ではなくopacityを切り替えるように変更

--- src/components/ScatterPlot/PlotPoints.jsx
+++ src/components/ScatterPlot/PlotPoints.jsx
@@ -2,12 +2,18 @@ function PlotPoints({ data, xScale, yScale, color, visibleSpecies }) {
   return (
     <g>
       {data.map(({ x, y, species }, i) => {
-        if (visibleSpecies[species]) {
-          return (
-            <circle key={i} cx={xScale(x)} cy={yScale(y)} r="5" fill={color[species]} />
-          )
-        }
-        return null
+        return (
+          <circle
+            key={i}
+            cx={xScale(x)}
+            cy={yScale(y)}
+            r="5"
+            fill={color[species]}
+            style={{
+              opacity: visibleSpecies[species] ? 1 : 0
+            }}
+          />
+        )
       })}
     </g>
   )

331d3202d66bac1ebe6a5afa13e61927ff51ffd7
kazari4 <skidyuk86115@gmail.com>
2025-06-03 01:39:04 +0900

    凡例にクラス名を追加

--- src/components/ScatterPlot/PlotLegend.jsx
+++ src/components/ScatterPlot/PlotLegend.jsx
@@ -19,6 +19,7 @@ function PlotLegend({ speciesList, color, visibleSpecies, setVisibleSpecies }) {
             }}
             transform={`translate(20,${30 * i})`}
             onClick={() => switchVisibleSpecies(setVisibleSpecies, species)}
+            className="legends"
           >
             <rect x="0" y="0" width={boxSize} height={boxSize} fill={color[species]} />
             <text x="15" y={boxSize} >{species}</text>

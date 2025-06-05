03cba6c455d0f50c3a2e16de5de5a09802d91800
kazari4 <skidyuk86115@gmail.com>
2025-06-05 13:10:17 +0900

    タイトルを変更

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
     <header className="hero is-info is-small">
       <div className="hero-body">
         <div className="container">
-          <h1 className="title has-text-weight-bold">Iris dataset scatter plot</h1>
+          <h1 className="title has-text-weight-bold">Scatter Plot of Iris Data</h1>
         </div>
       </div>
     </header>
